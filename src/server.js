c11520163993e19769128d58253a4241bbee9155
Johnny Araújo <ijohnnysa@gmail.com>
2020-10-05 21:35:40 -0300

    refactor: receive port from dotenv file

--- src/server.js
+++ src/server.js
@@ -1,8 +1,9 @@
+import 'dotenv/config';
 import app from './app';
 
 const server = app;
 
-const port = 3333;
+const port = process.env.APP_PORT;
 
 server.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
